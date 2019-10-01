5ba450ca38c813456bb89efcad8d5c4e23b289e0
MattRob77 <54182638+MattRob77@users.noreply.github.com>
2019-10-01 19:21:14 -0400

    Started building out interface structure

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,10 +8,16 @@ def run
   attractions = [] 
   puts "Welcome to NYC top attractions!"
   puts "Would you like to see the most popular attractions?"
+  puts "Please select a top attraction"
+  puts "Sorry, try again"
+  puts "Here are the details for #{}:"
+  puts "Highlights: #{}"
+  puts "Location: #{}"
+  puts "Amenities: #{}"
   html = open("https://www.citypass.com/new-york/things-to-do-new-york")
   doc = Nokogiri::HTML(html)
-  doc.css(".sidebar-attraction-nav").css("p").each do |attraction|
-    binding.pry
-  end 
- end
+  doc.css(".sidebar-attraction-nav").css("p").each do |a|
+   binding.pry
+   end 
+end 
 
