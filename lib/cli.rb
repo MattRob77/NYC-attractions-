67b01490e2010b5ca2cd1eccc0ccc381444d86c4
MattRob77 <54182638+MattRob77@users.noreply.github.com>
2019-10-01 15:21:02 -0400

    Installed gems/worked on CSS

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,11 +1,17 @@
 require "open-uri"
 require "nokogiri"
+require "pry"
+
 
 
 def run 
+  attractions = [] 
   puts "Welcome to NYC top attractions!"
-  puts "What would you like to do?"
-  html = open("https://www.nycgo.com/")
+  puts "Would you like to see the most popular attractions?"
+  html = open("https://www.citypass.com/new-york/things-to-do-new-york")
   doc = Nokogiri::HTML(html)
+  doc.css(".sidebar-attraction-nav").css("p").each do |attraction|
+    binding.pry
+  end 
+ end
 
-end 
