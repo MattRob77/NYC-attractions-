560e7a7d65123e83d729ac6afe136238512131a9
MattRob77 <54182638+MattRob77@users.noreply.github.com>
2019-10-01 23:57:58 -0400

    Update cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,22 +1,21 @@
-require "open-uri"
-require "nokogiri"
-require "pry"
+#User controller-input 
 
-
-def run 
-  attraction = []
-  puts "Welcome to NYC top attractions!"
-  puts "Would you like to see the most popular attractions?"
-  puts "Please select a top attraction"
-  puts "Sorry, try again"
-  puts "Here are the details for #{}"
-  puts "Highlights: #{}"
-  puts "Location: #{}"
-  html = open("https://www.citypass.com/new-york/things-to-do-new-york")
-  doc = Nokogiri::HTML(html)
-  doc.css("div.sidebar-attraction-nav p").each do |paragraph|  
-    attraction << paragraph.css("a").text
+class CLI 
+  def run 
+  attraction =[]
+    puts "Welcome to NYC top attractions!"
+    puts "Would you like to see the most popular attractions?"
+    puts "Please select a top attraction"
+    puts "Sorry, try again"
+    puts "Here are the details for #{}"
+    puts "Highlights: #{}"
+    puts "Location: #{}"
+    html = open("https://www.citypass.com/new-york/things-to-do-new-york")
+    doc = Nokogiri::HTML(html)
+    doc.css("div.sidebar-attraction-nav p").each do |paragraph|  
+      attraction << paragraph.css("a").text
+     end 
+    puts attraction 
   end 
-  puts attraction 
 end 
 
