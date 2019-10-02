d12b7e0d265237b771768c9223bdf32aafe234a7
MattRob77 <54182638+MattRob77@users.noreply.github.com>
2019-10-02 09:02:21 -0400

    Update cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,8 +1,9 @@
 #User controller-input 
+require "open-uri" 
+require "nokogiri"
 
 class CLI 
   def run 
-  attraction =[]
     puts "Welcome to NYC top attractions!"
     puts "Would you like to see the most popular attractions?"
     puts "Please select a top attraction"
@@ -10,15 +11,7 @@ class CLI
     puts "Here are the details for #{}"
     puts "Highlights: #{}"
     puts "Location: #{}"
-    html = open("https://www.citypass.com/new-york/things-to-do-new-york")
-    doc = Nokogiri::HTML(html)
-    doc.css("div.sidebar-attraction-nav p").each do |paragraph|  
-      attraction << paragraph.css("a").text
-     end 
-    
-    attraction.each.with_index(1) do |attraction, i| #These two lines of code sets the index, making attractions numbered 
-      puts "#{i} #{attraction}"  #numbered list 
-    end 
-  end 
+    Scraper.scrape_citypass 
+   end 
   
-end 
+end  
