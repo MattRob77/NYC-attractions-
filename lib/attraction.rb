7eb326ba34d1441aedcb4d921ac48f4456a03aef
MattRob77 <54182638+MattRob77@users.noreply.github.com>
2019-10-02 18:14:31 -0400

    added updated attribute accessors

--- lib/attraction.rb
+++ lib/attraction.rb
@@ -1,15 +1,16 @@
 require "open-uri"
 require "nokogiri"
+require "pry"
 
 
 class Attraction 
   
-  attr_accessor :headline, :url 
+  attr_accessor :headline, :url, :headline, :location, :contact
    @@all = []
  
   
   def initialize(headline, url)
-    @headline = headline 
+    @headline = headline
     @url = url 
     @@all << self 
   end 
