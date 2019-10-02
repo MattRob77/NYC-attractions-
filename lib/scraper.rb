1d6a2a5ff1df9114e522069788189f2f5facd812
MattRob77 <54182638+MattRob77@users.noreply.github.com>
2019-10-02 11:51:10 -0400

    Update scraper.rb

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,19 +1,22 @@
+
 require "open-uri"
 require "nokogiri"
 
 
+
 class Scraper 
- 
   
+
   def self.scrape_citypass
    attraction = [] 
+ 
     
     html = open("https://www.citypass.com/new-york/things-to-do-new-york")
     doc = Nokogiri::HTML(html)
     doc.css("div.sidebar-attraction-nav p").each do |paragraph|  
       attraction << paragraph.css("a").text
      end 
-     
+   
      attraction.each.with_index(1) do |attraction, i|
       puts "#{i} #{attraction}"
       end 
