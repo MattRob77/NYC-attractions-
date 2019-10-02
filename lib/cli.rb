dc0d59d5de68ad1afd02fa9e337b53b8b260e9df
MattRob77 <54182638+MattRob77@users.noreply.github.com>
2019-10-01 20:40:50 -0400

    Update cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,23 +1,23 @@
 require "open-uri"
 require "nokogiri"
-require "pry"
+
 
 
 
 def run 
-  attractions = [] 
+  array = []
   puts "Welcome to NYC top attractions!"
   puts "Would you like to see the most popular attractions?"
   puts "Please select a top attraction"
   puts "Sorry, try again"
-  puts "Here are the details for #{}:"
+  puts "Here are the details for #{}"
   puts "Highlights: #{}"
   puts "Location: #{}"
-  puts "Amenities: #{}"
   html = open("https://www.citypass.com/new-york/things-to-do-new-york")
   doc = Nokogiri::HTML(html)
-  doc.css(".sidebar-attraction-nav").css("p").each do |a|
-   binding.pry
-   end 
+  doc.css("div.sidebar-attraction-nav p").each do |paragraph|  
+    array << paragraph.css("a").text
+  end 
+  puts array
 end 
 
