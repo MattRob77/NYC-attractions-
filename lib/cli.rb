a25f4d105b10fb093a0d80076d38dcd53e7787b4
MattRob77 <54182638+MattRob77@users.noreply.github.com>
2019-10-03 15:55:56 -0400

    Update cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,17 +5,26 @@
 class CLI 
  
   def run 
-    puts "Welcome to NYC top attractions!"
+    puts "Welcome to NYC top attractions!" #Step 1 
     puts "Would you like to see the most popular attractions?"
     Scraper.scrape_citypass 
+    attractions 
     menu 
   end 
+  
+  
+  def attractions 
+       Attraction.all.each.with_index(1) do |attraction, i|
+      puts "#{i} #{attraction.headline}"
+      end 
+    end 
 
    
    def menu 
      puts "Please select a top attraction."
      input = gets.chomp #grabs input from the user (y,n)
      attraction = Attraction.all[input.to_i-1]
+   
     
      
      if !attraction
@@ -23,7 +32,7 @@ class CLI
        menu 
      else 
        Scraper.scrape_individual_attraction(attraction)
-       puts "Here are the details for #{attraction.paragraph}"
+       puts "Here are the details for #{attraction.headline}"
        puts "highlights: #{attraction.highlights}"
        puts "location: #{attraction.location}"
        puts "contact: #{attraction.contact}"
