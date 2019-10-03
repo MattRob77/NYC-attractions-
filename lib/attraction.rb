8dc1d530be3921996cf1095c2b2f09706f49a855
MattRob77 <54182638+MattRob77@users.noreply.github.com>
2019-10-03 19:02:05 -0400

    Added #notes/gave more explanation to code

--- lib/attraction.rb
+++ lib/attraction.rb
@@ -5,17 +5,18 @@ require "nokogiri"
 
 class Attraction 
   
-  attr_accessor :headline, :url, :contact, :location, :highlights
-   @@all = []
+  attr_accessor :headline, :url, :contact, :location, :highlights #attribute accessor 
+   @@all = [] #all to empty array 
  
   
-  def initialize(headline,url)
+  def initialize(headline,url) #initialize method with headline, url argument 
     @headline = headline
     @url = url 
-    @@all << self 
+    @@all << self #shoveled self = method into @@all 
   end 
   
   def self.all 
     @@all 
   end 
 end 
+
